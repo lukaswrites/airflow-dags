4a92282bdcbbfc0352152ea76ab8a5be82143bdf
lukaswrites123 <lukas.sahardiman@fundingsocieties.com>
2020-10-12 10:39:47 +0700

    add dags

---
+++ dag_builder.py
@@ -0,0 +1,22 @@
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
+
+
+#dynamically build dummy dags and tasks
+
+def long_process():
+    time.sleep(100)
+
+for i in range(10):
+    dag = DAG(
+        'dag_id'+i,
+        default_args=default_args,
+        description='dummy dag',
+        schedule_interval=timedelta(minutes=1)
+    )
+    for j in range(10):
+        task = PythonOperator(
+            task_id = 'task_id_'+j,
+            python_callable=long_process,
+            dag=dag
+        )
