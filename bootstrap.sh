6f4bc2f962a66571e8689f24cb416e14050a6cd2
lukaswrites123 <lukas.sahardiman@fundingsocieties.com>
2020-10-13 17:22:48 +0700

    add boot

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ sudo apt-get install --assume-yes python-psycopg2
 sudo apt-get install --assume-yes libpq-dev
 sudo apt install --assume-yes libcurl4-openssl-dev libssl-dev
 sudo apt-get install --assume-yes gcc python3.8-dev
+cp airflow-dags/requirements.txt .
 pip install -r requirements.txt
 airflow
-git clone https://github.com/lukaswrites/airflow-dags.git
 cp airflow-dags/airflow.cfg airflow
