71c1fae3abea8c240ba68cf2233528bb5e290f49
lukaswrites123 <lukas.sahardiman@fundingsocieties.com>
2020-10-13 14:54:41 +0700

    add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install --assume-yes python3.8
+sudo apt-get install --assume-yes python3.8-venv
+python3.8 -m venv env
+source env/bin/activate
+sudo apt-get install --assume-yes postgresql
+sudo apt-get install --assume-yes python-psycopg2
+sudo apt-get install --assume-yes libpq-dev
+sudo apt install --assume-yes libcurl4-openssl-dev libssl-dev
+sudo apt-get install --assume-yes gcc python3.8-dev
+pip install -r requirements.txt
+airflow
+git clone https://github.com/lukaswrites/airflow-dags.git
+cp airflow-dags/airflow.cfg airflow
