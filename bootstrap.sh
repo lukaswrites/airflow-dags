d629432f58e4211b0a2fba84cc299886ed5f9a76
lukaswrites123 <lukas.sahardiman@fundingsocieties.com>
2020-10-14 12:13:29 +0700

    c

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,8 @@ sudo apt install --assume-yes libcurl4-openssl-dev libssl-dev
 sudo apt-get install --assume-yes gcc python3.8-dev
 cp airflow-dags/requirements.txt .
 pip install -r requirements.txt
+su ubuntu
+source /env/bin/activate
 airflow
 cp airflow-dags/airflow.cfg /home/ubuntu/airflow/airflow.cfg
 airflow worker
