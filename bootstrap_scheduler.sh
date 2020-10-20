8d2b9a5cbad15a4ad53b472cfe70671d1af6646b
lukaswrites123 <lukas.sahardiman@fundingsocieties.com>
2020-10-20 21:52:15 +0700

    add

--- bootstrap_scheduler.sh
+++ bootstrap_scheduler.sh
@@ -2,16 +2,15 @@
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install --assume-yes python3.8
 sudo apt-get install --assume-yes python3.8-venv
+python3.8 -m venv env
+source env/bin/activate
 sudo apt-get install --assume-yes postgresql
 sudo apt-get install --assume-yes python-psycopg2
 sudo apt-get install --assume-yes libpq-dev
 sudo apt install --assume-yes libcurl4-openssl-dev libssl-dev
 sudo apt-get install --assume-yes gcc python3.8-dev
 cp airflow-dags/requirements.txt .
-sudo apt install --assume-yes curl
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.8 get-pip.py
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 mkdir -p /home/cloud_user/repo/airflow-dags/
 cp -R /airflow-dags /home/cloud_user/repo/
 cp /airflow-dags/airflow.cfg ~/airflow/airflow.cfg
