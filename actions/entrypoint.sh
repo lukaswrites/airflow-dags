ea6e4105065d36f31cd2156d48bbe1d714f5f033
lukaswrites123 <lukas.sahardiman@fundingsocieties.com>
2020-11-02 11:28:56 +0700

    add

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -3,6 +3,7 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+git fetch origin
 git checkout -b $GITHUB_HEAD_REF origin/$GITHUB_HEAD_REF
 git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --name-only --cherry-pick --pretty=format: remotes/origin/$GITHUB_BASE_REF...remotes/origin/$GITHUB_HEAD_REF > commit_list.txt
 cat commit_list.txt
