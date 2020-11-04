874a57198abfb53974e314e0d2877cfc932cd3cb
lukaswrites <lukas.sahardiman@fundingsocieties.com>
2020-11-04 09:55:49 +0700

    add

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -7,3 +7,4 @@ git fetch origin
 git checkout -b $GITHUB_HEAD_REF origin/$GITHUB_HEAD_REF
 git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --name-only --cherry-pick --pretty=format: remotes/origin/$GITHUB_BASE_REF...remotes/origin/$GITHUB_HEAD_REF > commit_list.txt
 cat commit_list.txt
+python3 sql_check.py commit_list.txt
